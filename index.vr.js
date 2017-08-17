e8b946bb4b6695d97221e4bc4697ec81ea32c7e2
atulsingla82 <asingla@wgu.edu>
2017-08-17 00:18:45 -0400

    Adding weather object for  current weather ( NYC )

--- index.vr.js
+++ index.vr.js
@@ -7,6 +7,8 @@ import {
   View
 } from 'react-vr';
 
+import WeatherCard from './vr/components/WeatherCard';
+
 const api_key = 'eba77ad8c254e0a32edcc15b5ff2fc45';
 
 class WeatherSimulator_ReactVR extends Component {
@@ -14,25 +16,44 @@ class WeatherSimulator_ReactVR extends Component {
   constructor(){
   super();
   this.state = {
-     place:'lombard-vr.jpg',
-     weatherObject: {}
+     place:'nyc.jpg',
+     weatherObject: {
+      name: "",
+      main:{
+        temp:0
+      },
+      weather:[
+      
+      {description: ""}
+
+      ],
+      wind:{
+       deg:1,
+       speed:1
+      }
+
+
+
+     }
   }
  }
 
  componentDidMount(){
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${api_key}`,{
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${api_key}`,
+  {
     method:'GET'
   })
   .then(response => response.json())
-  .then(json=> this.setState({weatherObject:json}));
+  .then(json=> this.setState({weatherObject:json}))
   }
 
   render() {
     return (
-      <View>
+      <View style={{ flex:1 , justifyContent:'center', alignItems:'center'}}>
       <Pano source={asset(this.state.place)}/>
+      <WeatherCard weatherObject = {this.state.weatherObject}/>
       </View>
-    );
+    )
   }
 };
 
