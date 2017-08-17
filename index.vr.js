5e8ab86951a049cd3b4bb3e7876b19ffcf8cbd70
atulsingla82 <asingla@wgu.edu>
2017-08-16 23:38:50 -0400

    Added API key

--- index.vr.js
+++ index.vr.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   asset,
   Pano,
   Text,
-  View,
+  View
 } from 'react-vr';
 
-export default class WeatherSimulator_ReactVR extends React.Component {
+const api_key = 'eba77ad8c254e0a32edcc15b5ff2fc45';
+
+class WeatherSimulator_ReactVR extends Component {
+
+  constructor(){
+  super();
+  this.state = {
+     place:'lombard-vr.jpg',
+     weatherObject: {}
+  }
+ }
+
+ componentDidMount(){
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${api_key}`,{
+    method:'GET'
+  })
+  .then(response => response.json())
+  .then(json=> this.setState({weatherObject:json}));
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+      <Pano source={asset(this.state.place)}/>
       </View>
     );
   }
