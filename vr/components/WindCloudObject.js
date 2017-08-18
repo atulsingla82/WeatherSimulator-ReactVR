f58a82d0a46a8f2865eae31abe503f19510901e3
atulsingla82 <asingla@wgu.edu>
2017-08-18 00:49:15 -0400

    Updated cloud animation

--- vr/components/WindCloudObject.js
+++ vr/components/WindCloudObject.js
@@ -6,9 +6,7 @@ export default class WindCloudObject extends Component{
 
 	constructor(){
      super();
-
      this.state = {
-
      	xValue: new Animated.Value(100)
      }
 
@@ -17,20 +15,29 @@ export default class WindCloudObject extends Component{
 	}
 
 	componentDidMount(){
-     
+
      this.animateCloud();
 
 	}
 
 
 	animateCloud(){
-      Animated.timing(this.state.xValue,{
-       toValue:-400,
-       duration:30000
+		if (this.state.xValue._value < 0 ){
+         this.setState({xValue: new Animated.Value(100)})
+
+		} else {
+          
+        Animated.timing(this.state.xValue,{
+       toValue:-1000,
+       duration:30000/this.props.wind.speed
 
       }).start();
 
+		}
+    
 	}
+
+
  render(){
    return (
     
@@ -38,7 +45,7 @@ export default class WindCloudObject extends Component{
     style= {{
     
     transform:[
-     { translate: [0, 25, -150]},
+     {translate: [0, 25, -150]},
      {rotateX: -90},
      {rotateZ:this.props.wind.deg},
      {translateX:this.state.xValue}
